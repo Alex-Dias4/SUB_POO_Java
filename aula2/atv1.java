1bc876b9a02b7179439714c88dd63f6b3c583ab5
Alexandre Dias <alexandre.xandedias@gmail.com>
2026-03-20 11:00:25 +0000

    atualização

---
+++ aula2/atv1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int valor = sc.next.Int()
+
+      if (valor>=10 && valor<=20){
+        System.out.println("O valor é maior ou igual a 10 e menor que 20")
+      } 
+}
---
+++ aula2/atv2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int valor = sc.next.Int()
+
+      if (valor>0){
+        System.out.println("É da positivo");
+      } else if(valor<0){
+        System.out.println("É da negativo");
+      }else{
+        System.out.println("É 0");
+      }
+}
